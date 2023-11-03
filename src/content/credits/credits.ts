1681384bf425f707c39e180a504a2cf1e8649366
trickypr <23250792+trickypr@users.noreply.github.com>
2023-11-03 19:25:37 +1100

    📄 Add license headers

--- src/content/credits/credits.ts
+++ src/content/credits/credits.ts
@@ -1,5 +1,8 @@
-import '../global.css'
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import '../global.css'
 
 import Credits from './Credits.svelte'
 
