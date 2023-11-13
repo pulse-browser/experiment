3c119cef02897d82530676675576e027bf1109a7
trickypr <23250792+trickypr@users.noreply.github.com>
2023-11-13 22:30:41 +1100

    👷 Add unit tests to CI

--- scripts/unit-test.ts
+++ scripts/unit-test.ts
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 import { argv, exit } from 'node:process'
 import { App } from '@tinyhttp/app'
 import { type ExecaChildProcess, execa } from 'execa'
